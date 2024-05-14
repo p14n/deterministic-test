2607d420225123afaf6149a2a9904d134f7fa5fa
Dean Chapman <dean.chapman@kroo.com>
2024-05-14 07:56:21 +0100

    Separate function and spec for re-run

--- test/core_test.clj
+++ test/core_test.clj
@@ -1,10 +1,7 @@
 (ns core-test
-  (:require [clojure.test :refer [deftest is testing]]
-            [core :as c]
-            [clojure.test.check :as tc]
+  (:require [core :as c]
             [clojure.test.check.generators :as gen]
             [clojure.test.check.clojure-test :refer [defspec]]
-            [clojure.test.check.results :as tcr]
             [clojure.test.check.properties :as prop])
   (:import (java.util Random Collections ArrayList)))
 
@@ -22,14 +19,9 @@
   (fn [event]
     (handler event data)))
 
-(defrecord Result [pass data]
-  tcr/Result
-  (pass? [_] pass)
-  (result-data [_] data))
-
 
 (defspec run-handlers-in-expected-order
-  10
+  20
   (prop/for-all
    [verified? gen/boolean
     blacklisted? gen/boolean]
@@ -62,51 +54,63 @@
     (Collections/shuffle a r)
     (vec (.toArray a))))
 
-(defn ensure-event-in-context [handler-execution-context event handlers seed]
-  (if (contains? handler-execution-context event)
-    handler-execution-context
-    (assoc handler-execution-context event
-           ;handlers
+(defn ensure-event-in-tracker [handler-execution-tracker event handlers seed]
+  (if (contains? handler-execution-tracker event)
+    handler-execution-tracker
+    (assoc handler-execution-tracker event
            (shuffle seed handlers))))
 
-(defn first-handler-from-context [handler-execution-context event]
-  (let [handlers (handler-execution-context event)]
-    [(first handlers) (assoc handler-execution-context event (rest handlers))]))
+(defn first-handler-from-tracker [handler-execution-tracker event handlers seed]
+  (let [updated-tracker (ensure-event-in-tracker handler-execution-tracker event handlers seed)
+        handlers (updated-tracker event)]
+    [(first handlers)
+     (assoc updated-tracker event (rest handlers))
+     (empty? (rest handlers))]))
 
 
-(defspec run-handlers-in-random-order
+(defn run-handlers-in-random-order
+  [verified? blacklisted? events-shuffle-seed handlers-shuffle-seed]
+  (let [cmd {:type :create-customer-requested
+             :customer-id "123"
+             :email "nobody@nowhere.com"}
+        all-handlers [(wrap-handler c/email-blacklist-check-sender nil)
+                      (wrap-handler c/verification-email-sender nil)
+                      (wrap-handler c/email-verifier verified?)
+                      (wrap-handler c/email-blacklist-checker blacklisted?)
+                      (wrap-handler c/customer-repository nil)]]
+    (reset! c/customer {})
+    (loop [events [cmd]
+           processed-events []
+           handler-execution-tracker {}]
+      (let [first-event (first events)
+            [handler updated-tracker event-fully-processed?] (first-handler-from-tracker
+                                                              handler-execution-tracker
+                                                              first-event
+                                                              all-handlers
+                                                              handlers-shuffle-seed)
+            new-events (->> (when (not event-fully-processed?) first-event)
+                            (conj [(handler first-event)])
+                            (concat (rest events))
+                            (remove nil?)
+                            (shuffle events-shuffle-seed))]
+        (if (empty? new-events)
+          (let [final-processed-events (conj processed-events first-event)
+                expected-email-status (expected-customer-email-status final-processed-events)
+                actual-email-status (:email-status @c/customer)]
+            {:pass (= expected-email-status actual-email-status)
+             :expected-email-status expected-email-status
+             :actual-email-status actual-email-status
+             :final-processed-events final-processed-events})
+          (recur new-events (if event-fully-processed?
+                              (conj processed-events first-event)
+                              processed-events)
+                 updated-tracker))))))
+
+(defspec run-handlers-in-random-order-spec
   (prop/for-all [verified? gen/boolean
                  blacklisted? gen/boolean
                  events-shuffle-seed gen/large-integer
                  handlers-shuffle-seed gen/large-integer]
-
-                (let [cmd {:type :create-customer-requested
-                           :customer-id "123"
-                           :email "nobody@nowhere.com"}
-                      all-handlers [(wrap-handler c/email-blacklist-check-sender nil)
-                                    (wrap-handler c/verification-email-sender nil)
-                                    (wrap-handler c/email-verifier verified?)
-                                    (wrap-handler c/email-blacklist-checker blacklisted?)
-                                    (wrap-handler c/customer-repository nil)]]
-                  (reset! c/customer {})
-                  (loop [events [cmd]
-                         processed-events []
-                         handler-execution-context {}]
-                    (let [first-event (first events)
-                          handler-ec-with-event (ensure-event-in-context handler-execution-context first-event all-handlers handlers-shuffle-seed)
-                          [handler updated-ec] (first-handler-from-context handler-ec-with-event first-event)
-                          event-fully-processed? (nil? (first (first-handler-from-context updated-ec first-event)))
-                          new-events (->> [(handler first-event) (when (not event-fully-processed?) first-event)]
-                                          (concat (rest events))
-                                          (remove nil?)
-                                          (shuffle events-shuffle-seed))]
-                      (if (empty? new-events)
-                        (let [final-processed-events (conj processed-events first-event)]
-                          (->Result (= (expected-customer-email-status final-processed-events) (:email-status @c/customer))
-                                    {:expected-email-status (expected-customer-email-status final-processed-events)
-                                     :actual-email-status (:email-status @c/customer)
-                                     :final-processed-events final-processed-events}))
-                        (recur new-events (if event-fully-processed?
-                                            (conj processed-events first-event)
-                                            processed-events)
-                               updated-ec)))))))
+                (-> (run-handlers-in-random-order verified? blacklisted? events-shuffle-seed handlers-shuffle-seed)
+                    :pass)))
+                
